d5109ffe86a7f7968b9b64b18ef6d3cf7e1960b1
Deyan <drs600@inlumine.ual.es>
2022-12-20 13:07:00 +0100

    structure update part 2 (docker + mongo)

--- services/apiv1ratingsidRatingService.js
+++ services/apiv1ratingsidRatingService.js
@@ -1,3 +1,8 @@
+import Rating from "../mongo/rating.js";
+import {Types} from 'mongoose';
+import {logger} from "@oas-tools/commons";
+import _ from 'lodash';
+
 export function findByidRating(req, res) {
     res.send({
         message: 'This is the mockup controller for findByidRating'
