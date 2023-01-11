863aaee6ce0aba8a8d379d06a2793c53a5b8b5bc
Deyan <drs600@inlumine.ual.es>
2023-01-11 19:44:09 +0100

    fixed all endpoints..

--- services/ratingManager.js
+++ services/ratingManager.js
@@ -2,116 +2,133 @@ import Rating from "../mongo/rating.js";
 import { Types } from "mongoose";
 import { logger } from "@oas-tools/commons";
 import _ from "lodash";
-import * as perspective from '../services/perspective.js';
+import * as perspective from "../services/perspective.js";
 import { CircuitBreaker } from "../circuitBreaker/circuitBreaker.js";
 
-
 export async function findByRecipeId(req, res) {
-  
   const recipe = req.params.idRecipe;
-  
-  CircuitBreaker.getBreaker(Rating).fire("find", {idRecipe: recipe}).then(result => {
-    
-    if(result) {
-      res.send(result);
 
-    } else {
-      res.sendStatus(404);
-    }
-    
-  }).catch((err) => {
-    res.sendStatus(500).send({ error: err.message });
-  })
+  CircuitBreaker.getBreaker(Rating)
+    .fire("find", { idRecipe: recipe })
+    .then((result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      res.sendStatus(500).send({ error: err.message });
+    });
 }
 
 export async function findByUserId(req, res) {
-
   const user = req.params.idUser;
-  
-  CircuitBreaker.getBreaker(Rating).fire("find", {idUser: user}).then(result => {
-    
-    if(result) {
-      res.send(result);
 
-    } else {
-      res.sendStatus(404);
-    }
-    
-  }).catch((err) => {
-    res.sendStatus(500).send({ error: err.message });
-  })
+  CircuitBreaker.getBreaker(Rating)
+    .fire("find", { idUser: user })
+    .then((result) => {
+      if (result) {
+        var resultsIdsOnly = [];
 
+        result.forEach(function (r, index) {
+          resultsIdsOnly.push(r.idRecipe);
+        });
+
+        res.send(resultsIdsOnly);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      res.sendStatus(500).send({ error: err.message });
+    });
 }
 
 export async function updateRating(req, res) {
   const valueLike = req.body.like;
-  const valueComment = req.body.comment;
-  const rating = req.params._id;
+  let valueComment = req.body.comment;
+  const idRating = req.params.idRating;
+  const idUser = req.body.idUser;
+  const idRecipe = req.body.idRecipe;
 
-  const validationResult = await perspective.validateRating(valueComment);
-  if (validationResult > 0.5) {
-    valueComment = "This comment has been removed due to toxicity";
-  }
+  try {
+    const validationResult = await perspective.validateRating(valueComment);
+    if (validationResult > 0.5) {
+      valueComment = "This comment has been removed due to toxicity";
+    }
 
-  var existingRating = await Rating.findOne({
-    _id: req.params.idRating,
-  });
-
-  if (existingRating != null) {
-
-    CircuitBreaker.getBreaker(Rating).fire("findByIdAndUpdate", {idRating: rating}, {like: valueLike, comment: valueComment}).then(result => {
-      if(result) {
-        res.sendStatus(201);
-
-      } else {
-        res.sendStatus(404);
-      }
-
-    }).catch ((err) => {
-      res.status(500).send({ errror: err.message });
-    })
+      CircuitBreaker.getBreaker(Rating)
+        .fire(
+          "findByIdAndUpdate",
+          { _id: idRating },
+          { like: valueLike, comment: valueComment, idUser: idUser, idRecipe: idRecipe }
+        )
+        .then((result) => {
+          if (result) {
+            res.sendStatus(204);
+          } else {
+            res.sendStatus(404);
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({ errror: err.message });
+        });
+    
+  } catch (err) {
+    res.status(400).send({ error: err.message });
   }
 }
 
 export async function deleteRating(req, res) {
-
   const rating = req.params.idRating;
 
-    try {
-      CircuitBreaker.getBreaker(Rating).fire("findByIdAndDelete", {idRating: rating});
-      return res.sendStatus(201);
-
-      } catch (err) {
-        res.status(400).send({ error: err.message });
-      }
+  try {
+    CircuitBreaker.getBreaker(Rating).fire("findByIdAndDelete", {
+      _id: rating,
+    });
+    return res.sendStatus(204);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
 }
 
 export async function getAllRatings(req, res) {
-  
-  CircuitBreaker.getBreaker(Rating).fire("find", {}).then((result) => {
-    res.send(result);
-    
-  }).catch((err) => {
-    res.status(500).send({ error: err.message });
-  })
+  CircuitBreaker.getBreaker(Rating)
+    .fire("find", {})
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({ error: err.message });
+    });
 }
 
 export async function addRating(req, res) {
-
   const newRating = req.body;
 
-      try {
+  try {
+    const validationResult = await perspective.validateRating(
+      newRating.comment
+    );
 
-        const validationResult = await perspective.validateRating(newRating.comment);
+    if (validationResult > 0.5) {
+      newRating.comment = "This comment has been removed due to toxicity";
+    }
 
-        if (validationResult > 0.5) {
-          newRating.comment = "This comment has been removed due to toxicity";
+    CircuitBreaker.getBreaker(Rating)
+      .fire("create", newRating)
+      .then((newRating) => {
+        if (addRating) {
+          return res.send(newRating);
+        } else {
+          res.sendStatus(404);
         }
-
-        CircuitBreaker.getBreaker(Rating).fire("create", newRating);
-        return res.send(newRating);
-        
-      } catch (err) {
-        res.status(400).send({ error: err.message });
-      }
+      })
+      .catch((err) => {
+        res.status(500).send({ error: err.message });
+      });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
 }
