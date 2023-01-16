d269e6fffc6afa1c8c5056478ac6c0b81c191cec
Deyan <drs600@inlumine.ual.es>
2023-01-16 22:20:31 +0100

    tests fixed

--- tests/components/components.js
+++ tests/components/components.js
@@ -1,5 +1,5 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
+import chai from "chai";
+import chaiHttp from "chai-http";
 
 chai.use(chaiHttp);
 chai.should();
@@ -9,113 +9,134 @@ var idRating = "";
 var idUser = "testUser";
 var comment = "test_comment";
 var like = true;
-let ratingPOST = { idUser: idUser, idRecipe: idRecipe, like: like, comment: comment }
-let ratingPUT = { idUser: idUser, idRecipe: idRecipe, like: false, comment: "edited" }
+let ratingPOST = {
+  idUser: idUser,
+  idRecipe: idRecipe,
+  like: like,
+  comment: comment,
+};
+let ratingPUT = {
+  idUser: idUser,
+  idRecipe: idRecipe,
+  like: false,
+  comment: "edited",
+};
 
-const apiURL = "http://localhost:8080"
+const apiURL = "http://localhost:8080";
 
-describe('/GET ratings', () => {
-    it('should GET all ratings', (done) => {
-        chai.request(apiURL)
-        .get('/api/v1/ratings')
-        .end((err, res) => {
-            res.body.should.be.a('array');
-            res.should.have.status(200);
-            chai.expect(res.body).to.have.length.greaterThan(0);
-            done();
-        })
-    })
-})
+describe("/GET ratings", () => {
+  before(() => {
+    // Wait for the service to start
+    let delay = new Promise((resolve) => setTimeout(resolve, 3000));
+    return delay;
+  });
+  it("should GET all ratings", (done) => {
+    chai
+      .request(apiURL)
+      .get("/api/v1/ratings")
+      .end((err, res) => {
+        res.body.should.be.a("array");
+        res.should.have.status(200);
+        chai.expect(res.body).to.have.length.greaterThan(0);
+        done();
+      });
+  });
+});
 
+describe("POST rating", () => {
+  before(() => {
+    // Wait for the service to start
+    let delay = new Promise((resolve) => setTimeout(resolve, 3000));
+    return delay;
+  });
 
-describe('POST rating', () => {
-    it('should post a rating', (done) => {
-        chai.request(apiURL)
-        .post('/api/v1/ratings')
-        .send(ratingPOST)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('idUser');
-            res.body.should.have.property('idRecipe');
-            res.body.should.have.property('like');
-            res.body.should.have.property('comment');
-            res.body.should.have.property('_id');
-            done();
+  it("should post a rating", (done) => {
+    chai
+      .request(apiURL)
+      .post("/api/v1/ratings")
+      .send(ratingPOST)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("idUser");
+        res.body.should.have.property("idRecipe");
+        res.body.should.have.property("like");
+        res.body.should.have.property("comment");
+        res.body.should.have.property("_id");
+        done();
 
-            idRating = res.body._id;
-        })
-    })
-})
+        idRating = res.body._id;
+      });
+  });
+});
 
+describe("GET/idRecipe ratings", () => {
+  it("should GET ratings by id recipe", (done) => {
+    chai
+      .request(apiURL)
+      .get("/api/v1/ratings/findByRecipeId/" + idRecipe)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.forEach((rating) => {
+          rating.should.have.property("idUser").eql(ratingPOST.idUser);
+          rating.should.have.property("idRecipe").eql(ratingPOST.idRecipe);
+          rating.should.have.property("like").eql(ratingPOST.like);
+          rating.should.have.property("comment").eql(ratingPOST.comment);
+        });
+        done();
+      });
+  });
+});
 
-describe('GET/idRecipe ratings', () => {
-    it('should GET ratings by id recipe', (done) => {
-        chai.request(apiURL)
-        .get('/api/v1/ratings/findByRecipeId/'+idRecipe)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            res.body.forEach(rating => {
-                rating.should.have.property('idUser').eql(ratingPOST.idUser);
-                rating.should.have.property('idRecipe').eql(ratingPOST.idRecipe);
-                rating.should.have.property('like').eql(ratingPOST.like);
-                rating.should.have.property('comment').eql(ratingPOST.comment);
-                
-            });
-            done();
-        })
-    })
-    
-})
+describe("GET/idUser ratings", () => {
+  it("should GET ratings by id user", (done) => {
+    chai
+      .request(apiURL)
+      .get("/api/v1/ratings/findByUserId/" + idUser)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
 
-describe('GET/idUser ratings', () => {
-    it('should GET ratings by id user', (done) => {
-        chai.request(apiURL)
-        .get('/api/v1/ratings/findByUserId/' + idUser)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            
-            done();
-        })
-    })
-})
+        done();
+      });
+  });
+});
 
+describe("PUT Rating", () => {
+  it("should update rating", (done) => {
+    chai
+      .request(apiURL)
+      .put("/api/v1/ratings/" + idRating)
+      .send(ratingPUT)
+      .end((err, res) => {
+        res.should.have.status(204);
+        done();
+      });
+  });
+});
 
-describe('PUT Rating', () => {
-    it('should update rating', (done) => {
-        chai.request(apiURL)
-        .put('/api/v1/ratings/' + idRating)
-        .send(ratingPUT)
-        .end((err, res) => {
-            res.should.have.status(204);
-            done();
-        })
-    })
-})
+describe("PUT Rating that does not exist", () => {
+  it("should fail to update rating", (done) => {
+    chai
+      .request(apiURL)
+      .put("/api/v1/ratings/ratingdoesnotexist")
+      .send(ratingPUT)
+      .end((err, res) => {
+        res.should.have.status(500);
+        done();
+      });
+  });
+});
 
-describe('PUT Rating that does not exist', () => {
-    it('should fail to update rating', (done) => {
-        chai.request(apiURL)
-        .put('/api/v1/ratings/ratingdoesnotexist')
-        .send(ratingPUT)
-        .end((err, res) => {
-            res.should.have.status(500);
-            done();
-        })
-    })
-})
-
-
-describe('DELETE Rating', () => {
-    it('should delete rating', (done) => {
-        chai.request(apiURL)
-        .delete('/api/v1/ratings/'+idRating)
-        .end((err, res) => {
-            res.should.have.status(204);
-            done();
-        })
-    })
-})
-
+describe("DELETE Rating", () => {
+  it("should delete rating", (done) => {
+    chai
+      .request(apiURL)
+      .delete("/api/v1/ratings/" + idRating)
+      .end((err, res) => {
+        res.should.have.status(204);
+        done();
+      });
+  });
+});
--- tests/integration/integration.js
+++ tests/integration/integration.js
@@ -1,127 +1,123 @@
-import chai, { expect } from 'chai';
-import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
+import chai, { expect } from "chai";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
 
 chai.use(sinonChai);
 
 const ratingController = {
-    deleteRating: sinon.stub().resolves(),
-    findByRecipeId: sinon.stub().resolves(),
-    findByUserId: sinon.stub().resolves(),
-    getAllRatings: sinon.stub().resolves(),
-    updateRating: sinon.stub().resolves(),
-    addRating: sinon.stub().resolves(),
+  deleteRating: sinon.stub().resolves(),
+  findByRecipeId: sinon.stub().resolves(),
+  findByUserId: sinon.stub().resolves(),
+  getAllRatings: sinon.stub().resolves(),
+  updateRating: sinon.stub().resolves(),
+  addRating: sinon.stub().resolves(),
 };
 
-describe('ratingController.getAllRatings()', () => {
-    it('should call ratingController.getAllRatings()', async () => {
-      const req = {
-        query: {
-        }
-      };
+describe("ratingController.getAllRatings()", () => {
+  it("should call ratingController.getAllRatings()", async () => {
+    const req = {
+      query: {},
+    };
 
-      const res = {
-        send: sinon.stub()
-      };
-  
-      await ratingController.getAllRatings(req, res);
-      expect(ratingController.getAllRatings).to.have.been.calledWith(req, res);
-    });
+    const res = {
+      send: sinon.stub(),
+    };
+
+    await ratingController.getAllRatings(req, res);
+    expect(ratingController.getAllRatings).to.have.been.calledWith(req, res);
+  });
 });
 
+describe("ratingController.addRating()", () => {
+  it("should call ratingController.addRating() with the correct arguments", () => {
+    const req = {
+      body: {
+        idRecipe: "1234",
+        idUser: "testUser",
+        like: true,
+        comment: "test comment",
+      },
+    };
 
-describe('ratingController.addRating()', () => {
-    it('should call ratingController.addRating() with the correct arguments', () => {
-      const req = {
-        body: {
-            idRecipe: "1234",
-            idUser: "testUser", 
-            like: true, 
-            comment: "test comment"
-        }
+    const res = {
+      send: sinon.stub(),
+    };
 
-      };
-
-      const res = {
-        send: sinon.stub()
-      };
-  
-      ratingController.addRating(req, res);
-      expect(ratingController.addRating).to.have.been.calledWith(req, res);
-    });
+    ratingController.addRating(req, res);
+    expect(ratingController.addRating).to.have.been.calledWith(req, res);
+  });
 });
 
-describe('ratingController.findByUserId()', () => {
-    it('should call ratingController.findByUserId() with the correct arguments', async () => {
-      const req = {
-        params: {
-          id: '12345'
-        }
-      };
+describe("ratingController.findByUserId()", () => {
+  it("should call ratingController.findByUserId() with the correct arguments", async () => {
+    const req = {
+      params: {
+        id: "12345",
+      },
+    };
 
-      const res = {
-        send: sinon.stub()
-      };
-  
-      await ratingController.findByUserId(req, res);
-      expect(ratingController.findByUserId).to.have.been.calledWith(req, res);
-    });
+    const res = {
+      send: sinon.stub(),
+    };
+
+    await ratingController.findByUserId(req, res);
+    expect(ratingController.findByUserId).to.have.been.calledWith(req, res);
+  });
 });
 
-describe('ratingController.findByRecipeId()', () => {
-    it('should call ratingController.findByRecipeId() with the correct arguments', async () => {
-      const req = {
-        params: {
-          id: '12345'
-        }
-      };
+describe("ratingController.findByRecipeId()", () => {
+  it("should call ratingController.findByRecipeId() with the correct arguments", async () => {
+    const req = {
+      params: {
+        id: "12345",
+      },
+    };
 
-      const res = {
-        send: sinon.stub()
-      };
-  
-      await ratingController.findByRecipeId(req, res);
-      expect(ratingController.findByRecipeId).to.have.been.calledWith(req, res);
-    });
+    const res = {
+      send: sinon.stub(),
+    };
+
+    await ratingController.findByRecipeId(req, res);
+    expect(ratingController.findByRecipeId).to.have.been.calledWith(req, res);
+  });
 });
 
-describe('ratingController.updateRating()', () => {
-    it('should call ratingController.updateRating() with the correct arguments', async () => {
-      const req = {
-        params: {
-          id: '12345'
-        },
-        body: {
-            idRecipe: "1111",
-            idUser: "testUser2", 
-            like: false, 
-            comment: "test comment 2"
-        }
-      };
+describe("ratingController.updateRating()", () => {
+  it("should call ratingController.updateRating() with the correct arguments", async () => {
+    const req = {
+      params: {
+        id: "12345",
+      },
+      body: {
+        idRecipe: "1111",
+        idUser: "testUser2",
+        like: false,
+        comment: "test comment 2",
+      },
+    };
 
-      const res = {
-        send: sinon.stub()
-      };
-  
-      await ratingController.updateRating(req, res);
-      expect(ratingController.updateRating).to.have.been.calledWith(req, res);
-    });
+    const res = {
+      send: sinon.stub(),
+    };
+
+    await ratingController.updateRating(req, res);
+    expect(ratingController.updateRating).to.have.been.calledWith(req, res);
+  });
 });
 
-describe('ratingController.deleteRating()', () => {
-    it('should call ratingController.deleteRating() with the correct arguments', async () => {
-        const req = {
-            params: {
-                id: '12345'
-            }
-        };
+describe("ratingController.deleteRating()", () => {
+  it("should call ratingController.deleteRating() with the correct arguments", async () => {
+    const req = {
+      params: {
+        id: "12345",
+      },
+    };
 
-        const res = {
-            send: sinon.stub()
-        };
-  
-        await ratingController.deleteRating(req, res);
-        expect(ratingController.deleteRating).to.have.been.calledWith(req, res);
-    });
+    const res = {
+      send: sinon.stub(),
+    };
+
+    await ratingController.deleteRating(req, res);
+    expect(ratingController.deleteRating).to.have.been.calledWith(req, res);
+  });
 });
-
